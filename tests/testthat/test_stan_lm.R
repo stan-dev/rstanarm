fa2a12652b0289018b7d0218e5df81e92f7255ab
Ben Goodrich <goodrich.ben@gmail.com>
2015-07-09 11:52:52 -0400

    test_stan_lm.R: weaken threshold

--- tests/testthat/test_stan_lm.R
+++ tests/testthat/test_stan_lm.R
@@ -1,10 +1,10 @@
 # tests can be run using devtools::test() or manually by loading testthat 
-# package and then running the code below.
+# package and then running the code below possibly with options(mc.cores = 4).
 
 library(rstanarm)
 set.seed(123)
 
-threshold <- 0.2
+threshold <- 0.3
 
 f1 <- function(x) cbind(coef(x), se(x))
 f2 <- function(x) summary(x)$coefficients[,1:2]
