a524774b070921d85705d8c1d8a4a906ac434c64
jgabry <jgabry@gmail.com>
2015-08-16 15:38:12 -0400

    add package doc page
    We can change this package, but this is a decent placeholder

---
+++ R/rstanarm-package.R
@@ -0,0 +1,36 @@
+#' Applied Regression Modeling via RStan
+#'
+#' @docType package
+#' @name rstanarm
+#' @import rstan
+#' 
+#' @description An extension to the \code{\link[rstan]{rstan}} package enabling
+#'   the most commonly applied regression models to be specified using customary
+#'   R modeling syntax. The user can choose to estimate a model by optimization
+#'   or perform full Bayesian inference via Markov chain Monte Carlo sampling.
+#' 
+#' @section \code{\link{stan_lm}}: 
+#' 
+#'   Linear modeling with regularizing priors on the model parameters that are
+#'   driven by prior beliefs about \eqn{R^2}, the proportion of variance in the
+#'   outcome attributable to the predictors. See \code{\link{priors}} for an
+#'   explanation of this critical point.
+#'   
+#' @section \code{\link{stan_glm}}:
+#'   
+#'   Generalized linear modeling with Gaussian, Student t, or Cauchy prior
+#'   distributions for the coefficients.
+#'   
+#' @section \code{\link{stan_glmer}} (and \code{stan_lmer}):
+#'   
+#'   Generalized linear modeling with group-specific terms with Gaussian,
+#'   Student t, or Cauchy prior distributions for the coefficients and flexible
+#'   priors for the unknown covariance matrices.
+#'   
+#' @section \code{\link{stan_polr}}:
+#'   
+#'   Ordinal regression modeling with Gaussian, Student t, or Cauchy prior
+#'   distributions for the coefficients.
+#'   
+#'   
+NULL
