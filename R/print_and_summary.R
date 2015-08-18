73d7f9a88afbed250cb1726855d9ef9d6cba794c
jgabry <jgabry@gmail.com>
2015-08-18 19:04:34 -0400

    in summary.stanreg, round n_eff, rename se_mean

--- R/print_and_summary.R
+++ R/print_and_summary.R
@@ -31,10 +31,17 @@ print.stanreg <- function(x, ...) {
   }
 }
 
-summary.stanreg <- function(object, ...) {
+summary.stanreg <- function(object, ..., digits = 2) {
   # use RStan's summary just as placeholder. we should replace this with our own
   # summary 
-  if(object$stanfit@mode == 0) summary(object$stanfit, ...)$summary
+  if (object$stanfit@mode == 0) {
+    out <- round(summary(object$stanfit, ...)$summary, digits = digits)
+    if ("n_eff" %in% colnames(out))
+      out[, "n_eff"] <- round(out[, "n_eff"])
+    if ("se_mean" %in% colnames(out)) # So people don't confuse se_mean and sd
+      colnames(out)[which(colnames(out) == "se_mean")] <- "mcse" 
+    out
+  }
   else {
     mark <- names(object$coefficients)
     if (object$family$family == "gaussian") mark <- c(mark, "sigma")
