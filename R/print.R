ddac23edf4c4443bf9d2059f659a411e6cd430e1
jgabry <jgabry@gmail.com>
2015-08-20 15:18:27 -0400

    print and summary in namespace

--- R/print.R
+++ R/print.R
@@ -1,3 +1,5 @@
+#' @method print stanreg
+#' @export
 print.stanreg <- function(x, ...) {
   # use RStan's print just as placeholder. we should replace this with our own
   # print method
@@ -28,24 +30,5 @@ print.stanreg <- function(x, ...) {
                               parameters = paste("Mean Sq", names(groups)))
     cat("ANOVA-like table\n")
     rstan::monitor(effects, warmup = 0, print = TRUE, digits_summary = 2)
-  }
-}
-
-summary.stanreg <- function(object, ..., digits = 2) {
-  # use RStan's summary just as placeholder. we should replace this with our own
-  # summary 
-  if (object$stanfit@mode == 0) {
-    out <- round(summary(object$stanfit, ...)$summary, digits = digits)
-    if ("n_eff" %in% colnames(out))
-      out[, "n_eff"] <- round(out[, "n_eff"])
-    if ("se_mean" %in% colnames(out)) # So people don't confuse se_mean and sd
-      colnames(out)[which(colnames(out) == "se_mean")] <- "mcse" 
-    out
-  }
-  else {
-    mark <- names(object$coefficients)
-    if (object$family$family == "gaussian") mark <- c(mark, "sigma")
-    else if (object$family$family == "Negative Binomial") mark <- c(mark, "overdispersion")
-    object$stan_summary[mark,,drop=FALSE]
   }
 }
