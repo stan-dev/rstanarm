c65163cb291a1796cea195416541b0b16447aa61
jgabry <jgabry@gmail.com>
2015-09-09 18:15:05 -0400

    temporary stan_glmer test
    I don’t really know how best to test these models. I’m skeptical about
    using optimization and sampling can be slow and give a decent number of
    warnings.

--- tests/testthat/test_stan_glmer.R
+++ tests/testthat/test_stan_glmer.R
@@ -7,14 +7,10 @@ options(mc.cores = 2L)
 if (interactive()) options(mc.cores = parallel::detectCores())
 set.seed(123)
 
-threshold <- 0.3
-
-f1 <- function(x) cbind(coef(x), se(x))
-f2 <- function(x) summary(x)$coefficients[,1:2]
-
 context("stan_glmer")
 test_that("stan_glmer returns expected result for slepstudy example", {
   # example using sleepstudy dataset
   fit <- stan_lmer(Reaction / 10 ~ Days + (Days | Subject), sleepstudy, init_r = 0.05)
   fm1 <- lmer(Reaction / 10 ~ Days + (Days | Subject), sleepstudy)
+  expect_equal(fixef(fit), fixef(fm1), tol = 0.05)
 })
