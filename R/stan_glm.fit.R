8f73265452553a32560eef4a31c7b7a6c47da9eb
Ben Goodrich <goodrich.ben@gmail.com>
2015-09-19 03:23:19 -0400

    utilize csr_matrix_times_vector()

--- R/stan_glm.fit.R
+++ R/stan_glm.fit.R
@@ -199,7 +199,11 @@ stan_glm.fit <- function(x, y, weights = rep(1, NROW(x)),
     standata$p <- as.array(p)
     standata$l <- as.array(l)
     standata$q <- ncol(Z)
-    standata$Z <- Z
+    parts <- extract_sparse_parts(Z)
+    standata$num_non_zero <- length(parts$w)
+    standata$w <- parts$w
+    standata$v <- parts$v
+    standata$u <- parts$u
     standata$gamma_shape <- as.array(maybe_broadcast(group$decov$gamma_shape, t))
     standata$scale <- as.array(maybe_broadcast(group$decov$scale, t))
     standata$len_concentration <- sum(p[p > 1])
@@ -213,7 +217,10 @@ stan_glm.fit <- function(x, y, weights = rep(1, NROW(x)),
     standata$p <- integer(0)
     standata$l <- integer(0)
     standata$q <- 0L
-    standata$Z <- matrix(0, nrow = nrow(xtemp), ncol = 0L)
+    standata$num_non_zero <- 0L
+    standata$w <- double(0)
+    standata$v <- integer(0)
+    standata$u <- integer(0)
     standata$gamma_shape <- standata$scale <- standata$concentration <-
       standata$shape <- rep(0, 0)
     standata$len_concentration <- 0L
