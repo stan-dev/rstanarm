06eb5ab7ac8575f68592d49651bcb0e79d4e1840
Ben Goodrich <goodrich.ben@gmail.com>
2015-10-18 13:17:55 -0400

    redo configuration again

--- tools/make_cpp.R
+++ tools/make_cpp.R
@@ -11,7 +11,7 @@ cat("#ifndef MODELS_HPP", "#define MODELS_HPP",
     return(cppcode)
     model_cppname <- paste0('model_', model_cppname)
     return(cppcode)
-  }), "#endif", file = "src/models.hpp", sep = "\n", append = FALSE)
+  }), "#endif", file = file.path("src", "include", "models.hpp"), sep = "\n", append = FALSE)
 
 options("useFancyQuotes" = FALSE)
 
@@ -24,7 +24,7 @@ sapply(sub(".stan", "", basename(stan_files), fixed = TRUE), function(f) {
                                 "grad_log_prob", "log_prob", 
                                 "unconstrain_pars", "constrain_pars", "num_pars_unconstrained", 
                                 "unconstrained_param_names", "constrained_param_names"), 
-                    file = paste0(f, "Module.cc"), header = '#include "models.hpp"', 
+                    file = paste0(f, "Module.cc"), header = '#include "include/models.hpp"', 
                     module = paste0("stan_fit4", f, "_mod"), 
                     CppClass = paste0("rstan::stan_fit<model_", f, "_namespace::model_", f,
                                       ", boost::random::ecuyer1988> "),
