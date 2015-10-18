ee025fe4d53e5a1f778f9d86cd94492ccc25654c
jgabry <jgabry@gmail.com>
2015-10-18 16:03:51 -0400

    fix sanity_check for bernoulli
    @bgoodri still not sure what to call this function (maybe pprefit or
    something like that?). i’m not sure it should be an argument to ppcheck
    since it’s not a plot.

--- R/sanity_check.R
+++ R/sanity_check.R
@@ -1,16 +1,15 @@
 #' Perform a sanity check
 #' 
-#' Refit a model using the same predictors but taking the outcome vector 
-#' \code{y} to be a draw from the \link[=posterior_predict]{posterior predictive
-#' distribution}. It can be useful to compare the estimates from the sanity
-#' check model to the estimates obtained from fitting the model to the real
-#' data. See Examples.
+#' Refit a model using the same predictors but replacing the outcome \code{y} 
+#' with data simulated from the \link[=posterior_predict]{posterior predictive 
+#' distribution}. It can be useful to compare the resulting estimates to the 
+#' estimates obtained from fitting the model to the observed data. See Examples.
 #' 
 #' @export
 #' @inheritParams stanreg-methods
 #' @param ... Arguments to or from other methods.
-#' @return The stanreg object resulting from fitting the model to the simulated
-#'   data.
+#' @return The \code{\link[=stanreg-objects]{stanreg}} object resulting from
+#'   fitting the model to the simulated data.
 #'   
 #' @seealso \code{\link{posterior_predict}}
 #' @examples 
@@ -25,8 +24,8 @@
 #' estimates <- cbind(fit = coef(fit), check = coef(fit_check), 
 #'                    se_fit = se(fit), se_check = se(fit_check))
 #' round(estimates)                    
-#' diffs <- summary(fit) - summary(fit_check)
-#' diffs[, colnames(diffs) %in% c("mean", "sd", "50%")]
+#' diffs <- round(summary(fit) - summary(fit_check), 1)
+#' diffs[, c("mean", "sd", "50%")]
 #' }  
 #' 
 sanity_check <- function(object, ...) {
@@ -40,7 +39,7 @@ sanity_check <- function(object, ...) {
     y <- get_y(object)
     if (NCOL(y) == 1L && !all(y %in% c(0, 1)))
       yrep <- yrep / object$weights
-    else 
+    if (NCOL(y) == 2L) 
       yrep <- cbind(yrep, rowSums(y) - yrep)
   }
   mf[[1L]] <- yrep
