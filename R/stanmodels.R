e54e835a2d35800f1029851a737ad58066ac63d3
Ben Goodrich <goodrich.ben@gmail.com>
2015-10-18 04:42:48 -0400

    stanmodels.R: consolidation

--- R/stanmodels.R
+++ R/stanmodels.R
@@ -23,44 +23,22 @@ loadModule("stan_fit4polr_mod", TRUE)
 MODELS_HOME <- "exec"
 if (!file.exists(MODELS_HOME)) MODELS_HOME <- sub("R$", "exec", getwd())
 
-stanfit_bernoulli <- rstan::stanc(file.path(MODELS_HOME, "bernoulli.stan"),
-                                  obfuscate_model_name = FALSE)
-stanfit_bernoulli$model_cpp <- list(model_cppname = stanfit_bernoulli$model_name, 
-                                    model_cppcode = stanfit_bernoulli$cppcode)
-stanfit_bernoulli <- do.call(new, args = c(stanfit_bernoulli[-(1:3)], Class = "stanmodel", 
-                                           mk_cppmodule = function(x) model_bernoulli))
+make_stanfit <- function(f) {
+  model_cppname <- sub("\\.stan$", "", basename(f))
+  program <- c(readLines(file.path(MODELS_HOME, "functions.txt")), 
+               readLines(f))
+  program <- paste(program, collapse = "\n")
+  stanfit <- rstan::stanc(model_code = program, model_name = model_cppname, 
+                          obfuscate_model_name = FALSE)
+  stanfit$model_cpp <- list(model_cppname = stanfit$model_name, 
+                            model_cppcode = stanfit$cppcode)
+  return(do.call(new, args = c(stanfit[-(1:3)], Class = "stanmodel", 
+                 mk_cppmodule = function(x) get(paste0("model_", model_cppname)))))
+}
 
-stanfit_binomial <- rstan::stanc(file.path(MODELS_HOME, "binomial.stan"),
-                                 obfuscate_model_name = FALSE)
-stanfit_binomial$model_cpp <- list(model_cppname = stanfit_binomial$model_name, 
-                                   model_cppcode = stanfit_binomial$cppcode)
-stanfit_binomial <- do.call(new, args = c(stanfit_binomial[-(1:3)], Class = "stanmodel", 
-                                          mk_cppmodule = function(x) model_binomial))
-
-stanfit_continuous <- rstan::stanc(file.path(MODELS_HOME, "continuous.stan"), 
-                                   obfuscate_model_name = FALSE)
-stanfit_continuous$model_cpp <- list(model_cppname = stanfit_continuous$model_name, 
-                                     model_cppcode = stanfit_continuous$cppcode)
-stanfit_continuous <- do.call(new, args = c(stanfit_continuous[-(1:3)], Class = "stanmodel", 
-                                            mk_cppmodule = function(x) model_continuous))
-
-stanfit_count <- rstan::stanc(file.path(MODELS_HOME, "count.stan"), 
-                              obfuscate_model_name = FALSE)
-stanfit_count$model_cpp <- list(model_cppname = stanfit_count$model_name, 
-                                model_cppcode = stanfit_count$cppcode)
-stanfit_count <- do.call(new, args = c(stanfit_count[-(1:3)], Class = "stanmodel", 
-                                            mk_cppmodule = function(x) model_count))
-
-stanfit_lm <- rstan::stanc(file.path(MODELS_HOME, "lm.stan"), 
-                           obfuscate_model_name = FALSE)
-stanfit_lm$model_cpp <- list(model_cppname = stanfit_lm$model_name, 
-                             model_cppcode = stanfit_lm$cppcode)
-stanfit_lm <- do.call(new, args = c(stanfit_lm[-(1:3)], Class = "stanmodel", 
-                                    mk_cppmodule = function(x) model_lm))
-
-stanfit_polr <- rstan::stanc(file.path(MODELS_HOME, "polr.stan"), 
-                           obfuscate_model_name = FALSE)
-stanfit_polr$model_cpp <- list(model_cppname = stanfit_polr$model_name, 
-                               model_cppcode = stanfit_polr$cppcode)
-stanfit_polr <- do.call(new, args = c(stanfit_polr[-(1:3)], Class = "stanmodel", 
-                                    mk_cppmodule = function(x) model_polr))
+stanfit_bernoulli <- make_stanfit(file.path(MODELS_HOME, "bernoulli.stan"))
+stanfit_binomial <- make_stanfit(file.path(MODELS_HOME, "binomial.stan"))
+stanfit_continuous  <- make_stanfit(file.path(MODELS_HOME, "continuous.stan"))
+stanfit_count <- make_stanfit(file.path(MODELS_HOME, "count.stan"))
+stanfit_lm <- make_stanfit(file.path(MODELS_HOME, "lm.stan"))
+stanfit_polr <- make_stanfit(file.path(MODELS_HOME, "polr.stan"))
