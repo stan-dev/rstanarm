dd31757f600eceb28c897019b6caa2abc5fdcdec
Ben Goodrich <goodrich.ben@gmail.com>
2015-10-19 18:04:47 -0400

    example_model.R: emphasize in the example that the sampling() arguments are only to reduce the size of the object

--- R/example_model.R
+++ R/example_model.R
@@ -18,6 +18,7 @@
 #' example_model <- 
 #'   stan_glmer(cbind(incidence, size - incidence) ~ size + period + (1|herd),
 #'              data = lme4::cbpp, family = binomial,
+#'              # this next line is only to keep the example small in size!
 #'              chains = 2, cores = 1, seed = 12345, iter = 500)
 #' }
 NULL
