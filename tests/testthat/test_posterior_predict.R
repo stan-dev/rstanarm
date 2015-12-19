3c11e4ce45be303af3ba2f3feb84f16ee744e672
jgabry <jgabry@gmail.com>
2015-12-19 18:03:48 -0500

    more posterior_predict tests

--- tests/testthat/test_posterior_predict.R
+++ tests/testthat/test_posterior_predict.R
@@ -108,7 +108,7 @@ test_that("posterior_predict close to predict.merMod for gaussian", {
   mod2 <- as.formula(mpg ~ log1p(wt) + I(disp/100) + (1|cyl))
   mod3 <- as.formula(mpg ~ wt + (1|cyl) + (1 + wt|gear))
   mod4 <- as.formula(log(mpg) ~ wt + (1 + wt|cyl) + (1 + wt + am|gear))
-
+  
   lfit1 <- lmer(mod1, data = mtcars)
   sfit1 <- stan_glmer(mod1, data = mtcars, cores = CORES, chains = CHAINS, 
                       iter = 400, seed = SEED)
@@ -125,35 +125,56 @@ test_that("posterior_predict close to predict.merMod for gaussian", {
   nd3$gear[2] <- 7
   nd3$gear[5] <- 1
   
+  tol <- 0.3
   for (j in 1:4) {
     expect_equal(
       predict(get(paste0("sfit", j))),
       unname(predict(get(paste0("lfit", j)))),
-      tol = 0.5)
+      tol = tol)
     expect_equal(
       predict(get(paste0("sfit", j)), newdata = nd),
       predict(get(paste0("lfit", j)), newdata = nd),
-      tol = 0.5)
+      tol = tol)
     expect_equal(
-      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd)),
+      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd, seed = SEED)),
       unname(predict(get(paste0("lfit", j)), newdata = nd)),
-      tol = 0.5)
+      tol = tol)
     expect_equal(
-      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd2, 
+      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd2, seed = SEED,
                                  allow.new.levels = TRUE)),
       unname(predict(get(paste0("lfit", j)), newdata = nd2, allow.new.levels = TRUE)),
-      tol = 0.5)
+      tol = tol)
     expect_equal(
-      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd3, 
+      colMeans(posterior_predict(get(paste0("sfit", j)), newdata = nd3, seed = SEED,
                                  allow.new.levels = TRUE)),
       unname(predict(get(paste0("lfit", j)), newdata = nd3, allow.new.levels = TRUE)),
-      tol = 0.5)
+      tol = tol)
     
     expect_error(
       posterior_predict(get(paste0("sfit", j)), newdata = nd3, 
                         allow.new.levels = FALSE),
       regexp = "new levels", ignore.case = TRUE)
   }
+})
+
+test_that("posterior_predict close to predict.merMod for binomial", {
+  d <- nd <- lme4::cbpp
+  sfit <- example_model
+  lfit <- glmer(formula(example_model), data = d, family = "binomial")
+  levels(nd$herd) <- c(levels(nd$herd), "99")
+  nd$herd[1:2] <- "99"
+  lpred <- simulate(lfit, newdata = nd, re.form = NULL, allow.new.levels = TRUE,
+                    nsim = 500, seed = SEED)
+  for (j in 1:ncol(lpred)) {
+    lpred[, j] <- lpred[, j][, 1] / rowSums(lpred[, j])
+  }
+  lpred <- t(as.matrix(lpred))
+  spred <- posterior_predict(sfit, draws = 500, newdata = nd, 
+                             allow.new.levels = TRUE, seed = SEED)
+  spred <- sweep(spred, 2, rowSums(get_y(sfit)), "/")
+  expect_equal(colMeans(spred), unname(colMeans(lpred)),
+               tol = .05)
+  max(abs(colMeans(spred) - colMeans(lpred)))
 })
 
 test_that("pp_new_levels works properly", {
@@ -171,6 +192,55 @@ test_that("pp_new_levels works properly", {
   expect_equal(dim(beta), c(500, 21))
   expect_equal(colnames(x)[1], "(Intercept)")
   expect_equal(colnames(x)[dim(x)[2]], "99")
-  expect_equal(colnames(beta), c(rownames(summary(sfit))[1:20], ""))
+  expect_equal(colnames(beta), rownames(summary(sfit))[1:ncol(beta)])
+})
+
+test_that("lme4 tests work similarly", {
+  # loosely following predict tests from lme4
+  
+  sfit <- example_model
+  nd <- lme4::cbpp
+  
+  p1 <- posterior_predict(sfit, seed = SEED)
+  p1b <- posterior_predict(sfit, newdata = nd, seed = SEED)
+  expect_equal(p1, p1b)
+  
+  p2 <- posterior_predict(sfit, re.form = NA, seed = SEED)
+  expect_equal(ncol(p2), nrow(nd))
+  
+  
+  nd2 <- with(nd, expand.grid(period = unique(period), 
+                              herd = unique(herd), 
+                              size = 20))
+  
+  p3 <- posterior_predict(sfit, nd2, seed = SEED)
+  p4 <- expect_silent(posterior_predict(sfit, nd2, re.form = NA, seed = SEED))
+  p5 <- posterior_predict(sfit, nd2, re.form = ~(1|herd), seed = SEED)
+  expect_equal(p3, p5)
+  
+  # new levels
+  nd3 <- rbind(nd2, data.frame(period = as.character(1:4), 
+                               herd = rep("new",4), 
+                               size = 20))
+  expect_error(posterior_predict(sfit, nd3), regexp = "new levels detected", 
+               ignore.case = TRUE)
+  
+  p6 <- posterior_predict(sfit, nd3, allow.new.levels = TRUE, seed = SEED)
+  expect_equal(colMeans(p3), colMeans(p6[, 1:ncol(p3)]), tol = 0.05)
+  expect_equal(apply(p3, 2, sd), apply(p6[, 1:ncol(p3)], 2, sd), tol = 0.05)
+  
+  # multiple groups
+  lfit <- lmer(diameter ~ (1|plate) + (1|sample), Penicillin)
+  sfit <- stan_lmer(diameter ~ (1|plate) + (1|sample), data = Penicillin, 
+                    cores = CORES, chains = CHAINS, 
+                    iter = 400, seed = SEED)
+ 
+  nd <- with(Penicillin, expand.grid(plate=unique(plate), sample=unique(sample)))
+  p1 <- posterior_predict(sfit, re.form = NA, seed = SEED)
+  p2 <- posterior_predict(sfit, nd, seed = SEED)
+  p3 <- posterior_predict(sfit, nd, re.form = NA, seed = SEED)
+  p4 <- posterior_predict(sfit, nd, re.form=~(1|plate)+(~1|sample), seed = SEED)
+  p4b <- posterior_predict(sfit, nd, re.form=~(1|sample)+(~1|plate), seed = SEED)
+  expect_equal(p2,p4,p4b)
 })
 
