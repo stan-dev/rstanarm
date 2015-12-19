285a99d9ca14834086df45eb3bc315e89ac53d48
Ben Goodrich <goodrich.ben@gmail.com>
2015-12-19 16:53:13 -0500

    neg_binomial_2.R: specify seed in example

--- R/neg_binomial_2.R
+++ R/neg_binomial_2.R
@@ -16,7 +16,7 @@
 #' @return An object of class \code{\link[stats]{family}} very similar to
 #'   that of \code{\link[stats]{poisson}} but with a different family name.
 #' @examples
-#' stan_glm(Days ~ Sex/(Age + Eth*Lrn), data = MASS::quine, 
+#' stan_glm(Days ~ Sex/(Age + Eth*Lrn), data = MASS::quine, seed = 12345,
 #'          family = neg_binomial_2, QR = TRUE, algorithm = "fullrank") 
 #'                 
 #' # or, equivalently, call stan_glm.nb() without specifying the family
