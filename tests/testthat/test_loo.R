5374f697d38f50648ddd7ccd8150bb29f50fc0ec
jgabry <jgabry@gmail.com>
2015-12-22 16:36:37 +0100

    test_posterior_predict.R: gamma and inverse gaussian

--- tests/testthat/test_loo.R
+++ tests/testthat/test_loo.R
@@ -67,10 +67,10 @@ test_that("loo/waic for stan_glm works", {
   expect_identical_loo(fit_negbin)
   
   # gamma
-  clotting <- data.frame(u = c(5,10,15,20,30,40,60,80,100),
+  clotting <- data.frame(log_u = log(c(5,10,15,20,30,40,60,80,100)),
                          lot1 = c(118,58,42,35,27,25,21,19,18),
                          lot2 = c(69,35,26,21,18,16,13,12,12))
-  fit_gamma <- stan_glm(lot1 ~ log(u), data = clotting, family = Gamma, 
+  fit_gamma <- stan_glm(lot1 ~ log_u, data = clotting, family = Gamma, 
                         chains = CHAINS, iter = ITER, 
                         cores = CORES, seed = SEED)
   expect_identical_loo(fit_gamma)
