dc21d5d5f68fb9aafd5cf1e51d423917ccc35451
Ben Goodrich <goodrich.ben@gmail.com>
2016-01-07 11:05:06 -0500

    posterior_interval.R: fix non-ASCII dash in references

--- R/posterior_interval.R
+++ R/posterior_interval.R
@@ -57,7 +57,7 @@
 #' @references 
 #' Morey, R. D., Hoekstra, R., Rouder, J., Lee, M. D., and Wagenmakers, E.
 #' (2015). The fallacy of placing confidence in confidence intervals.
-#' \emph{Psychonomic Bulletin & Review}. 1–21.
+#' \emph{Psychonomic Bulletin & Review}. 1 -- 21.
 #' 
 #' @examples 
 #' posterior_interval(example_model)
