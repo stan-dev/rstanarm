2c5bef4411d709905f5c9bbbd2df60ebffb0f87c
jgabry <jgabry@gmail.com>
2016-01-07 13:38:55 -0500

    update posterior_interval.R
    * default to prob=0.9
    * add to documentation

--- R/posterior_interval.R
+++ R/posterior_interval.R
@@ -30,27 +30,36 @@
 #' @template args-regex-pars
 #' @param prob A number between 0 and 1 indicating the desired posterior 
 #'   probability mass \eqn{p} to include in the intervals. The default is to 
-#'   report 90\% intervals (\code{prob=0.9}) rather than the traditionally used
-#'   95\%, although the latter can be computed by specifying \code{prob=0.95}. 
-#'   We use this default for several reasons:
-#'   \itemize{
-#'   \item Computational stability: 90\% intervals are more stable than 95\% 
-#'   intervals (for which each end relies on only 2.5\% of the posterior draws).
-#'   \item Relation to Type-S errors: for 95\% of the mass in a 90\% central 
-#'   interval is above the lower value and 95\% of the mass is below the upper 
-#'   value. It is therefore easy to see if the probability that a parameter 
-#'   \eqn{\theta > 0} or \eqn{\theta<0} is larger or smaller than 95\%. See
-#'   Gelman and Carlin (2014).
-#'   }
+#'   report 90\% intervals (\code{prob=0.9}) rather than the traditionally used 
+#'   95\% (see Details). If desired, 95\% intervals can be computed by 
+#'   specifying \code{prob=0.95}.
 #' @param type The type of interval to compute. Currently the only option is 
-#'   \code{"central"}, although other possibilities may be available in future
-#'   releases. A central \eqn{100p}\% interval is defined by the \eqn{\alpha}
-#'   and \eqn{1 - \alpha} quantiles, where \eqn{\alpha = (1 - p)/2}.
+#'   \code{"central"} (see Details). A central \eqn{100p}\%
+#'   interval is defined by the \eqn{\alpha} and \eqn{1 - \alpha} quantiles,
+#'   where \eqn{\alpha = (1 - p)/2}.
 #' @param pars An optional character vector of parameter names.
 #' @param ... Currently ignored.
 #' 
 #' @return A two-column matrix.
-#'   
+#' 
+#' @details
+#'  
+#' We default to reporting 90\% intervals rather than 95\% intervals for several reasons:
+#' \itemize{
+#'  \item Computational stability: 90\% intervals are more stable than 95\% 
+#'  intervals (for which each end relies on only 2.5\% of the posterior draws).
+#'  \item Relation to Type-S errors (Gelman and Carlin, 2014): 95\% of the 
+#'  mass in a 90\% central interval is above the lower value and 95\% of the 
+#'  mass is below the upper value. For a parameter \eqn{\theta}, it is therefore
+#'  easy to see if the posterior probability that \eqn{\theta > 0} (or
+#'  \eqn{\theta < 0}) is larger or smaller than 95\%.
+#' }
+#' 
+#' Currently \code{posterior_interval} only computes central intervals because
+#' other types of intervals are rarely useful for the models that \pkg{rstanarm}
+#' can estimate. Additional possibilities may be provided in future releases as
+#' more models become available.
+#' 
 #' @seealso \code{\link{confint.stanreg}}, which, for models fit using 
 #'   optimization, can be used to compute traditional confidence intervals.
 #'   
@@ -62,7 +71,7 @@
 #' posterior_interval(example_model, regex_pars = "herd")
 #' posterior_interval(example_model, pars = "period2", prob = 0.9)
 #' 
-posterior_interval <- function(object, prob = 0.5, type = "central",
+posterior_interval <- function(object, prob = 0.9, type = "central",
                     pars = NULL, regex_pars = NULL, ...) {
   if (!is.stanreg(object))
     stop(deparse(substitute(object)), " is not a stanreg object.")
--- man-roxygen/reference-gelman-carlin.R
+++ man-roxygen/reference-gelman-carlin.R
@@ -1,4 +1,4 @@
 #' @references 
-#' Gelman, A. and Carlin, J. (2014). Beyond power calculations: assessing Type S
-#' (sign) and Type M (magnitude) errors. \emph{Perspect Psychol Sci.}. 9(6),
-#' 641--51.
+#' Gelman, A. and Carlin, J. (2014). Beyond power calculations:
+#' assessing Type S (sign) and Type M (magnitude) errors. \emph{Perspectives on
+#' Psychological Science}. 9(6), 641--51.
