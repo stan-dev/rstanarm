7c5a1ee72ed1edea6ce0d62ddb08976a1f93db08
jgabry <jgabry@gmail.com>
2016-01-12 19:39:30 -0500

    Revert "add failing test for stan_glm with no intercept"
    This reverts commit e301f552b610cc31cbbbf0e4706bcde896bad641.

--- tests/testthat/test_stan_glm.R
+++ tests/testthat/test_stan_glm.R
@@ -99,14 +99,6 @@ test_that("stan_glm returns expected result for cars example", {
   ans <- glm(log(dist) ~ log(speed), data = cars, family = gaussian(link = "identity"))
   expect_equal(coef(fit), coef(ans), tol = 0.04)
 })
-test_that("stan_glm returns expected result with no intercept for mtcars example", {
-  fit <- stan_glm(mpg ~ -1 + ., data = mtcars, 
-                  family = gaussian(link = "identity"), seed  = SEED,
-                  prior = NULL, prior_intercept = NULL, prior_ops = NULL,
-                  tol_rel_obj = .Machine$double.eps, algorithm = "optimizing")
-  ans <- glm(mpg ~ -1 + ., data = mtcars, family = gaussian(link = "identity"))
-  expect_equal(coef(fit), coef(ans), tol = 0.04)
-})
 
 context("stan_glm (bernoulli)")
 links <- c("logit", "probit", "cauchit", "log", "cloglog")
