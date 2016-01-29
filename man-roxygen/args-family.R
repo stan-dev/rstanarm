dc7618b93881eb6a30831fd5c21d211bc17eab68
jgabry <jgabry@gmail.com>
2016-01-29 18:15:13 -0500

    args-family.R: minor edits to doc

--- man-roxygen/args-family.R
+++ man-roxygen/args-family.R
@@ -3,8 +3,7 @@
 #'   calling \code{\link[<%= pkg %>]{<%= pkgfun %>}}, e.g., \code{family = 
 #'   gaussian(link = "log")}. The second method, a call to 
 #'   \code{\link{rstanarm_family}}, allows the user to override the default 
-#'   values for various family-specific parameters. Also, in addition to all of 
-#'   the traditionally available \code{\link[=family]{families}},
-#'   \pkg{rstanarm} supports families \code{\link{neg_binomial_2}} for
-#'   negative binomial models and \code{\link{t_family}} for linear models with
-#'   t-distributed errors.
+#'   values for various family-specific parameters. Also, in addition to the
+#'   traditional \code{\link[=family]{families}}, \pkg{rstanarm} supports
+#'   families \code{\link{neg_binomial_2}} for negative binomial models and
+#'   \code{\link{t_family}} for linear models with t-distributed errors.
