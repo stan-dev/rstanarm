c4f81b177fadfb37306f8c5378d4ce21aecc273b
jgabry <jgabry@gmail.com>
2016-01-29 17:13:38 -0500

    reduce MCMC output in demos
    also, for sections in ARM_Ch04 with a ton of output to console stop and
    ask if user wants to proceed or skip
    
    resolves #64

--- demo/ARM_Ch04.R
+++ demo/ARM_Ch04.R
@@ -11,16 +11,18 @@ source(paste0(ROOT, "ARM/Ch.4/earnings.data.R"), local = DATA_ENV, verbose = FAL
 # These two models are essentially equivalent in the likelihood
 # But the "same" priors affect the posterior differently
 post1 <- stan_glm(log(earn) ~ height, data = DATA_ENV, 
-                  family = gaussian(link = "identity"), seed = SEED)
+                  family = gaussian(link = "identity"), 
+                  seed = SEED, refresh = REFRESH)
 # post2 <- stan_glm(earn ~ height, data = DATA_ENV, 
-#                   family = gaussian(link = "log"), seed = SEED)
+#                   family = gaussian(link = "log"), 
+#                   seed = SEED, refresh = REFRESH)
 # and this does not even converge
 
 # These models add terms to the right-hand side
 post3 <- stan_lm(log(earn) ~ height + male, data = DATA_ENV,
-                 prior = R2(location = 0.3, what = "mean"), seed = SEED)
-post4 <- stan_lm(log(earn) ~ height * male, data = DATA_ENV,
-                 prior = R2(location = 0.3, what = "mean"), seed = SEED)
+                 prior = R2(location = 0.3, what = "mean"), 
+                 seed = SEED, refresh = REFRESH)
+post4 <- update(post3, formula = log(earn) ~ height * male)
 
 # Compare them with loo
 loo1 <- loo(post1)
@@ -48,35 +50,49 @@ axis(1, at = 1:ncol(y_men), labels = MEN_SEQ, las = 3)
 
 # Prediction of the weight of mesquite trees 
 source(paste0(ROOT, "ARM/Ch.4/mesquite.data.R"), local = DATA_ENV, verbose = FALSE)
-post5 <- stan_lm(weight ~ diam1 + diam2 + canopy_height + total_height +
-                   density + group, data = DATA_ENV,
-                 prior = R2(0.9), seed = SEED)
-post6 <- stan_lm(log(weight) ~ log(diam1) + log(diam2) + log(canopy_height) +
-                   log(total_height) + log(density) + group, data = DATA_ENV,
-                 prior = R2(0.9), seed = SEED)
-post7 <- stan_lm(log(weight) ~ log(diam1 * diam2 * canopy_height), 
-                 data = DATA_ENV, prior = R2(0.75, what = "mean"), seed = SEED)
-post8 <- stan_lm(log(weight) ~ log(diam1 * diam2 * canopy_height) + 
-                   log(diam1 * diam2) + group, data = DATA_ENV,
-                 prior = R2(0.8), seed = SEED)
-post9 <- stan_lm(log(weight) ~ log(diam1 * diam2 * canopy_height) + 
-                   log(diam1 * diam2) + log(diam1 / diam2) + group, 
-                 data = DATA_ENV, prior = R2(0.85), seed = SEED)
-
-compare(loo(post5), loo(post6), loo(post7), loo(post8), loo(post9))
+CONTINUE1 <- tolower(readline(
+  paste("A heads up: the next part of the demo (Predicting weight of mesquite trees )",
+        "prints many lines \nto the console as it runs many models and compares the results", 
+        "Proceed? (y/n)")
+))
+if (CONTINUE1 != "n") {
+  post5 <- stan_lm(weight ~ diam1 + diam2 + canopy_height + total_height +
+                     density + group, data = DATA_ENV,
+                   prior = R2(0.9), seed = SEED, refresh = REFRESH)
+  post6 <- update(post5, formula = log(weight) ~ log(diam1) + log(diam2) + 
+                    log(canopy_height) + log(total_height) + log(density) + group)
+  post7 <- update(post5, formula = log(weight) ~ log(diam1 * diam2 * canopy_height), 
+                  prior = R2(0.75, what = "mean"))
+  post8 <- update(post5, formula = log(weight) ~ log(diam1 * diam2 * canopy_height) + 
+                    log(diam1 * diam2) + group, prior = R2(0.8))
+  post9 <- update(post5, formula = log(weight) ~ log(diam1 * diam2 * canopy_height) + 
+                    log(diam1 * diam2) + log(diam1 / diam2) + group, 
+                  prior = R2(0.85))
+  
+  # Compare them with loo
+  compare(loo(post5), loo(post6), loo(post7), loo(post8), loo(post9))
+}
 
 # Predicting "continuous" party ID over time without multilevel stuff
-YEARS <- as.character(seq(from = 1972, to = 2000, by = 4))
-round(digits = 2, x = sapply(YEARS, FUN = function(YEAR) {
-  source(paste0(ROOT, "ARM/Ch.4/nes", YEAR, ".data.R"), local = DATA_ENV, verbose = FALSE)
-  coef(stan_lm(partyid7 ~ real_ideo + I(race_adj == 1) + as.factor(age_discrete) + 
-                 educ1 + gender + income, data = DATA_ENV, seed = SEED, 
-               prior = R2(0.5)))
-}))
+CONTINUE2 <- tolower(readline(
+  paste("A heads up: the next part of the demo (Predicting party ID over time)",
+        "prints many lines \nto the console as it runs many models and compares the results", 
+        "Proceed? (y/n)")
+))
+if (CONTINUE2 != "n") {
+  YEARS <- as.character(seq(from = 1972, to = 2000, by = 4))
+  round(digits = 2, x = sapply(YEARS, FUN = function(YEAR) {
+    source(paste0(ROOT, "ARM/Ch.4/nes", YEAR, ".data.R"), local = DATA_ENV, verbose = FALSE)
+    coef(stan_lm(partyid7 ~ real_ideo + I(race_adj == 1) + as.factor(age_discrete) + 
+                   educ1 + gender + income, data = DATA_ENV, prior = R2(0.5),
+                 seed = SEED, refresh = 0))
+  }))
+}
 
 ANSWER <- tolower(readline("Do you want to remove the objects this demo created? (y/n) "))
 if (ANSWER != "n") {
-  rm(WOMEN_SEQ, MEN_SEQ, y_women, y_men, YLIM, YEARS, ANSWER)
+  if (CONTINUE2 != "n") rm(YEARS)
+  rm(WOMEN_SEQ, MEN_SEQ, y_women, y_men, YLIM, ANSWER, CONTINUE1, CONTINUE2)
   # removes stanreg and loo objects, plus what was created by STARTUP
   demo("CLEANUP", package = "rstanarm", verbose = FALSE, echo = FALSE, ask = FALSE)
 }
--- demo/ARM_Ch14.R
+++ demo/ARM_Ch14.R
@@ -9,17 +9,18 @@ election88 <- with(DATA_ENV, data.frame(y, black, v.prev.full = v_prev_full,
 t_prior <- student_t(df = 7)
 fmla1 <- y ~ black + female + (1 | state)
 M1 <- stan_glmer(fmla1, data = election88, family = binomial(link="logit"),
-                 prior = t_prior, prior_intercept = t_prior, iter = 250)
+                 prior = t_prior, prior_intercept = t_prior, 
+                 seed = SEED, iter = 250, refresh = 125) # this model is a bit slow to run
 print(M1, digits = 2) # can also do fixef(M1), ranef(M1), VarCorr(M1), etc. 
 
 fmla2 <- y ~ black + female + black:female + v.prev.full + 
   (1 | age) + (1 | edu) + (1 | age.edu) + (1 | state) + (1 | region.full)
 M2 <- update(M1, formula = fmla2)
-print(M2, digits = 2)
+print(M2)
 
 ANSWER <- tolower(readline("Do you want to remove the objects this demo created? (y/n) "))
 if (ANSWER != "n") {
-  rm(election88, t_prior, fmla1, fmla2)
+  rm(election88, t_prior, fmla1, fmla2, ANSWER)
   # removes stanreg and loo objects, plus what was created by STARTUP
   demo("CLEANUP", package = "rstanarm", verbose = FALSE, echo = FALSE, ask = FALSE)
 }
