d7ec882fe86c7397ba17bec82b598e2a17ce4c13
jgabry <jgabry@gmail.com>
2016-01-29 00:01:23 -0500

    clarify that prior=NULL is (improper) uniform

--- man-roxygen/args-priors.R
+++ man-roxygen/args-priors.R
@@ -1,9 +1,11 @@
-#' @param prior Prior for coefficients. Can be \code{NULL} to omit a prior and 
-#'   see \code{\link{priors}} otherwise.
-#' @param prior_intercept Prior for intercept. Can be \code{NULL} to omit a 
-#'   prior and see \code{\link{priors}} otherwise. (\strong{Note:} the prior
-#'   distribution for the intercept is set so it applies to the value when all
-#'   predictors are centered.)
+#' @param prior Prior for coefficients. See \code{\link{priors}} for details. 
+#'   Set \code{prior} to \code{NULL} to omit a prior, i.e., use an (improper)
+#'   uniform prior.
+#' @param prior_intercept Prior for intercept. See \code{\link{priors}} for 
+#'   details. Set \code{prior_intercept} to \code{NULL} to omit a prior, i.e.,
+#'   use an (improper) uniform prior. (\strong{Note:} the prior distribution for
+#'   the intercept is set so it applies to the value when all predictors are 
+#'   centered.)
 #' @param prior_ops Additional options related to prior distributions. Can 
 #'   be \code{NULL} to omit a prior on the dispersion and see 
 #'   \code{\link{prior_options}} otherwise.
