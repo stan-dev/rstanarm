c547ba53f75cbf8c1d017e03b8a56bccc1fc9cea
jgabry <jgabry@gmail.com>
2016-02-08 18:05:40 -0500

    allow loading datasets for vignettes with data()

---
+++ R/datasets.R
@@ -0,0 +1,32 @@
+# Part of the rstanarm package for estimating model parameters
+# Copyright (C) 2015 Trustees of Columbia University
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#' Datasets for rstanarm examples
+#' 
+#' Datasets for use in \pkg{rstanarm} examples and vignettes.
+#'
+#' @name exdata
+#' @keywords internal
+#' @aliases kidiq roaches wells
+#' @format Data frames with the following dimensions:
+#' \describe{
+#' \item{\code{kidiq}}{434 obs. of 6 variables}
+#' \item{\code{roaches}}{262 obs. of 6 variables}
+#' \item{\code{wells}}{3020 obs. of 5 variables}
+#' }
+#' @source \url{http://stat.columbia.edu/~gelman/arm/}
+NULL
