941120c8807cc91c557d4964288165606c068449
jgabry <jgabry@gmail.com>
2016-03-17 14:53:30 -0700

    posterior_linpred: posterior distribution of linear predictor
    see #85

---
+++ R/posterior_linpred.R
@@ -0,0 +1,67 @@
+# Part of the rstanarm package for estimating model parameters
+# Copyright (C) 2015, 2016 Trustees of Columbia University
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#' Posterior distribution of the linear predictor
+#' 
+#' @export
+#' @templateVar stanregArg object
+#' @template args-stanreg-object
+#' @param newdata Optionally, a data frame in which to look for variables with 
+#'   which to predict. If omitted, the model matrix is used. If \code{newdata} 
+#'   is provided and any variables were transformed (e.g. rescaled) in the data 
+#'   used to fit the model, then these variables must also be transformed in 
+#'   \code{newdata}. This only applies if variables were transformed before 
+#'   passing the data to one of the modeling functions and \emph{not} if 
+#'   transformations were specified inside the model formula. Also see the Note
+#'   section below for a note about using the \code{newdata} argument with with
+#'   binomial models.
+#' @param draws An integer indicating the number of draws to return. The default
+#'   and maximum number of draws is the size of the posterior sample.
+#' @param re.form If \code{object} contains \code{\link[=stan_glmer]{group-level}}
+#'   parameters, a formula indicating which group-level parameters to 
+#'   condition on when making predictions. \code{re.form} is specified in the 
+#'   same form as for \code{\link[lme4]{predict.merMod}}. The default, 
+#'   \code{NULL}, indicates that all estimated group-level parameters are 
+#'   conditioned on. To refrain from conditioning on any group-level parameters,
+#'   specify \code{NA} or \code{~0}. The \code{newdata} argument may include new
+#'   \emph{levels} of the grouping factors that were specified when the model 
+#'   was estimated, in which case the resulting posterior predictions 
+#'   marginalize over the relevant variables.
+#' @param ... Currently unused.
+#' 
+#' @return A \code{draws} by \code{nrow(newdata)} matrix of simulations
+#'   from the posterior distribution of the linear predictor.
+#' 
+#' @seealso \code{\link{posterior_predict}} to draw from the posterior 
+#'   predictive distribution of the outcome.
+#' 
+posterior_linpred <- function(object, newdata = NULL, re.form = NULL, ...) {
+  if (!is.stanreg(object))
+    stop(deparse(substitute(object)), " is not a stanreg object.")
+  if (used.optimizing(object))
+    STOP_not_optimizing("posterior_predict")
+  if (!is.null(newdata)) {
+    if ("gam" %in% names(object))
+      stop("'posterior_predict' with 'newdata' not yet supported ", 
+           "for models estimated via 'stan_gamm4'.")
+    newdata <- as.data.frame(newdata)
+    if (any(is.na(newdata))) 
+      stop("Currently NAs are not allowed in 'newdata'.")
+  }
+  dat <- pp_data(object, newdata, re.form, ...)
+  pp_eta(object, dat, draws = NULL)[["eta"]]
+}
