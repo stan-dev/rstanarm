5ace9a9db0b26b749a4e4dd0421d09a94395edc6
jgabry <jgabry@gmail.com>
2016-04-01 15:44:06 -0400

    pp_validate.R: change . to _ in code

--- R/pp_validate.R
+++ R/pp_validate.R
@@ -51,9 +51,9 @@ pp_validate <- function(object, nreps, seed = 12345, ...) {
   # based on Samantha Cook's BayesValidate::validate
   quant <- function(draws) {
     n <- length(draws)
-    rank.theta <- c(1:n)[order(draws) == 1] - 1
-    quantile.theta <- (rank.theta +.5) / n
-    return(quantile.theta)
+    rank_theta <- c(1:n)[order(draws) == 1] - 1
+    quants <- (rank_theta + 0.5) / n
+    return(quants)
   }
   
   if (missing(nreps))
@@ -69,79 +69,79 @@ pp_validate <- function(object, nreps, seed = 12345, ...) {
     dims <- append(dims, values = vals, after = mark)
     dims <- dims[-mark]
   }
-  n.batch <- dims
-  params.batch <- names(dims)
-  num.batches <- length(n.batch)
-  n.param <- sum(dims)
-  batch.ind <- rep(0, num.batches + 1)
-  plot.batch <- rep(1, n.batch[1])
-  for (i in 1:num.batches) 
-    batch.ind[i+1] <- batch.ind[i] + n.batch[i]
-  for (i in 2:num.batches) 
-    plot.batch <- c(plot.batch, rep(i, n.batch[i]))
-  quantile.theta <- matrix(NA_real_, nrow = nreps, ncol = n.param + num.batches)
+  batches <- dims
+  params_batch <- names(dims)
+  num_batches <- length(batches)
+  num_params <- sum(dims)
+  batch_ind <- rep(0, num_batches + 1)
+  plot_batch <- rep(1, batches[1])
+  for (i in 1:num_batches) 
+    batch_ind[i+1] <- batch_ind[i] + batches[i]
+  for (i in 2:num_batches) 
+    plot_batch <- c(plot_batch, rep(i, batches[i]))
+  quantile_theta <- matrix(NA_real_, nrow = nreps, ncol = num_params + num_batches)
   for (reps in 1:nreps) {
-    post <- suppressWarnings(update(object, prior_PD = TRUE, 
-                 warmup = 1000, iter = 1000 + 2, chains = 1, seed = seed))
-    theta.true <- as.matrix(post)[1,, drop = FALSE]
-    data.rep <- posterior_predict(post)[1, ]
+    post <- suppressWarnings(update(object, prior_PD = TRUE, seed = seed,
+                                    warmup = 1000, iter = 1000 + 2, chains = 1))
+    theta_true <- as.matrix(post)[1,, drop = FALSE]
+    data_rep <- posterior_predict(post)[1, ]
     mf <- model.frame(object)
     if (NCOL(mf[, 1]) == 2) { # binomial models
-      mf[, 1] <- c(data.rep)
+      mf[, 1] <- c(data_rep)
       colnames(mf)[1] <- colnames(get_y(object))[1]
     } else {
-      mf[, 1] <- c(data.rep)
+      mf[, 1] <- c(data_rep)
     }
-    theta.draws <- as.matrix(update(object, data = mf, seed = seed))
-    if (!is.null(n.batch)){
-      for (i in 1:num.batches) {
-        if (n.batch[i] > 1) {
-          sel <- (batch.ind[i]+1):batch.ind[(i+1)]
-          theta.draws <-  cbind(theta.draws, 
-                                apply(theta.draws[, sel], 1, mean))
-          theta.true <- c(theta.true, mean(theta.true[sel]))
+    theta_draws <- as.matrix(update(object, data = mf, seed = seed))
+    if (!is.null(batches)){
+      for (i in 1:num_batches) {
+        if (batches[i] > 1) {
+          sel <- (batch_ind[i]+1):batch_ind[(i+1)]
+          theta_draws <-  cbind(theta_draws, 
+                                apply(theta_draws[, sel], 1, mean))
+          theta_true <- c(theta_true, mean(theta_true[sel]))
         } else {
-          theta.draws <- cbind(theta.draws, theta.draws[, (batch.ind[i]+1)])
-          theta.true <- c(theta.true, theta.true[(batch.ind[i]+1)])
+          theta_draws <- cbind(theta_draws, theta_draws[, (batch_ind[i]+1)])
+          theta_true <- c(theta_true, theta_true[(batch_ind[i]+1)])
         }
       }
     }
-    theta.draws <- rbind(theta.true, theta.draws)
-    quantile.theta[reps, ] <- apply(theta.draws, 2, quant)
+    theta_draws <- rbind(theta_true, theta_draws)
+    quantile_theta[reps, ] <- apply(theta_draws, 2, quant)
   }
-  quantile.trans <- (apply(quantile.theta, 2, qnorm))^2
-  q.trans <- apply(quantile.trans, 2, sum) 
-  p.vals <- pchisq(q.trans, df = nreps, lower.tail = FALSE)
-  z.stats <- abs(qnorm(p.vals))
-  if (is.null(n.batch)) {
-    adj.min.p <- n.param * min(p.vals)
+  quantile_trans <- (apply(quantile_theta, 2, qnorm))^2
+  q_trans <- apply(quantile_trans, 2, sum) 
+  p_vals <- pchisq(q_trans, df = nreps, lower.tail = FALSE)
+  z_stats <- abs(qnorm(p_vals))
+  if (is.null(batches)) {
+    adj_min_p <- num_params * min(p_vals)
   } else {
-    z.batch <- z.stats[(n.param + 1):length(p.vals)]
-    p.batch <- p.vals[(n.param + 1):length(p.vals)]
-    adj.min.p <- num.batches * min(p.batch)
+    z_batch <- z_stats[(num_params + 1):length(p_vals)]
+    p_batch <- p_vals[(num_params + 1):length(p_vals)]
+    adj_min_p <- num_batches * min(p_batch)
   }
   
-  upper.lim <- max(max(z.stats + 1), 3.5)
-  plotdata <- data.frame(x = z.batch, y = params.batch)
+  upper_lim <- max(max(z_stats + 1), 3.5)
+  plotdata <- data.frame(x = z_batch, y = params_batch)
   defaults <- list(shape = 21, fill = .PP_FILL, color = "black", 
                    size = 2.5, alpha = 1)
   geom_args <- set_geom_args(defaults, ...)
   ggplot(plotdata, aes(x, y)) + 
     geom_segment(aes(x = 0, xend = x, y = y, yend = y)) +
     do.call("geom_point", geom_args) + 
-    scale_x_continuous(limits = c(0, upper.lim), expand = c(0, 0)) + 
+    scale_x_continuous(limits = c(0, upper_lim), expand = c(0, 0)) + 
     labs(y = NULL, x = expression("Absolute " * z[theta] * " Statistics")) + 
     pp_check_theme(no_y = FALSE) + 
     theme(panel.grid.major.x = element_line(size = 0.1, color = "gray"))
   
   
-  # if (is.null(n.batch)){
-  #   return(list(p.vals = p.vals, adj.min.p = adj.min.p))
+  # if (is.null(batches)){
+  #   return(list(p_vals = p_vals, adj_min_p = adj_min_p))
   # } else {
-  #   if (length(z.batch) == n.param)
-  #     return(list(p.batch = p.batch, adj.min.p = adj.min.p)) 
+  #   if (length(z_batch) == num_params)
+  #     return(list(p_batch = p_batch, adj_min_p = adj_min_p)) 
   #   else 
-  #     return(list(p.vals = p.vals[1:n.param], p.batch = p.batch, 
-  #                 adj.min.p = adj.min.p))
+  #     return(list(p_vals = p_vals[1:num_params], p_batch = p_batch, 
+  #                 adj_min_p = adj_min_p))
   # }
 }
