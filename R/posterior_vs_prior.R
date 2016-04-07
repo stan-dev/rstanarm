bb353982d66111fc2b65c101d1654b5cc46438b0
Ben Goodrich <goodrich.ben@gmail.com>
2016-04-07 09:41:04 -0400

    posterior_vs_prior.R: fix some ggplot2 issues
    @jgabry: it looks fine but just needs a test

--- R/posterior_vs_prior.R
+++ R/posterior_vs_prior.R
@@ -45,9 +45,9 @@
 #' gg + ggplot2::scale_color_grey()
 #' gg + 
 #'  ggplot2::scale_color_brewer() + 
-#'  ggplot2::theme(panel.background = element_rect(fill = "gray30"))
+#'  ggplot2::theme(panel.background = ggplot2::element_rect(fill = "gray30"))
 #' 
-#' @importFrom ggplot2 geom_pointrange facet_wrap aes_string labs
+#' @importFrom ggplot2 geom_pointrange facet_wrap aes_string labs scale_x_discrete
 #' 
 posterior_vs_prior <- function(object,
                                pars = "beta",
