b178e70218bf7449322f25876fe1ce7f8ed203de
jgabry <jgabry@gmail.com>
2016-06-15 15:00:26 -0400

    edit doc

--- man-roxygen/args-priors.R
+++ man-roxygen/args-priors.R
@@ -3,14 +3,15 @@
 #'   \code{cauchy}, \code{hs} or \code{hs_plus}. See \code{\link{priors}} for
 #'   details. To to omit a prior ---i.e., to use a flat (improper) uniform
 #'   prior--- set \code{prior} to \code{NULL}.
-#' @param prior_intercept The prior distribution for the intercept.
-#'   \code{prior_intercept} can be a call to \code{normal}, \code{student_t} or
-#'   \code{cauchy}. See \code{\link{priors}} for details. To to omit a prior
-#'   ---i.e., to use a flat (improper) uniform prior--- set
-#'   \code{prior_intercept} to \code{NULL}. (\strong{Note:} the prior
-#'   distribution for the intercept is set so it applies to the value when all
-#'   predictors are centered if a dense representation of the design matrix
-#'   is utilized.)
+#' @param prior_intercept The prior distribution for the intercept. 
+#'   \code{prior_intercept} can be a call to \code{normal}, \code{student_t} or 
+#'   \code{cauchy}. See \code{\link{priors}} for details. To to omit a prior 
+#'   ---i.e., to use a flat (improper) uniform prior--- set 
+#'   \code{prior_intercept} to \code{NULL}. (\strong{Note:} if a dense 
+#'   representation of the design matrix is utilized ---i.e., if the
+#'   \code{sparse} argument is left at its default value of \code{FALSE}--- then
+#'   the prior distribution for the intercept is set so it applies to the value
+#'   when all predictors are centered.)
 #' @param prior_ops Additional options related to prior distributions. Can 
 #'   be \code{NULL} to omit a prior on the dispersion and see 
 #'   \code{\link{prior_options}} otherwise.
