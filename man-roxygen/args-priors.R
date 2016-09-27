d3ccfc3a61cb92b49eb582b9a029dc60e72c942f
jgabry <jgabry@gmail.com>
2016-09-27 14:40:49 -0400

    update doc on prior scales
    @bgoodri are you ok with these minor changes to the doc? I think
    hopefully this makes it a bit more clear to users what’s going on (I’ve
    been getting questions about this recently).

--- man-roxygen/args-priors.R
+++ man-roxygen/args-priors.R
@@ -1,8 +1,13 @@
 #' @param prior The prior distribution for the regression coefficients. 
-#'   \code{prior} can be a call to \code{normal}, \code{student_t},
-#'   \code{cauchy}, \code{hs} or \code{hs_plus}. See \code{\link{priors}} for
-#'   details. To to omit a prior ---i.e., to use a flat (improper) uniform
-#'   prior--- set \code{prior} to \code{NULL}.
+#'   \code{prior} can be a call to \code{normal}, \code{student_t}, 
+#'   \code{cauchy}, \code{hs} or \code{hs_plus}. See \code{\link{priors}} for 
+#'   details. To omit a prior ---i.e., to use a flat (improper) uniform prior---
+#'   \code{prior} can be set to \code{NULL}, although this is rarely a good
+#'   idea. (\strong{Note:} if the \code{scaled} argument to 
+#'   \code{\link{prior_options}} is left at its default and recommended value of
+#'   \code{TRUE}, then the scale(s) of \code{prior} may be modified internally
+#'   based on the scales of the predictors, as in the \pkg{arm} package. See
+#'   \code{\link{priors}} for details.)
 #' @param prior_intercept The prior distribution for the intercept. 
 #'   \code{prior_intercept} can be a call to \code{normal}, \code{student_t} or 
 #'   \code{cauchy}. See \code{\link{priors}} for details. To to omit a prior 
