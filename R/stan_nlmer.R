36d9895bf7ffa351fd73dcdd313c3996a0f26b0a
Ben Goodrich <goodrich.ben@gmail.com>
2016-12-27 10:17:55 -0500

    stan_nlmer.R: no prior_intercept

--- R/stan_nlmer.R
+++ R/stan_nlmer.R
@@ -39,14 +39,30 @@
 #'   \code{\link[rstan]{sampling}} (e.g. \code{iter}, \code{chains}, 
 #'   \code{cores}, etc.) or to \code{\link[rstan]{vb}} (if \code{algorithm} is 
 #'   \code{"meanfield"} or \code{"fullrank"}).
-#' @param prior The prior distribution for the regression "coefficients" (really
-#'   intercepts). \code{prior} can be a call to \code{normal}, \code{student_t},
-#'   \code{cauchy}, \code{hs} or \code{hs_plus}. See \code{\link{priors}} for
-#'   details. To to omit a prior ---i.e., to use a flat (improper) uniform
-#'   prior--- set \code{prior} to \code{NULL}.
-#' @param prior_ops Additional options related to prior distributions. Can 
-#'   be \code{NULL} to omit a prior on the dispersion and see 
-#'   \code{\link{prior_options}} otherwise.
+#' @param prior The prior distribution for the regression coefficients. 
+#'   \code{prior} can be a call to \code{normal}, \code{student_t}, 
+#'   \code{cauchy}, \code{hs} or \code{hs_plus}. See \code{\link{priors}} for 
+#'   details. To omit a prior ---i.e., to use a flat (improper) uniform prior---
+#'   \code{prior} can be set to \code{NULL}, although this is rarely a good 
+#'   idea. (\strong{Note:} Unless \code{QR=TRUE}, if \code{prior} is specified 
+#'   as \code{normal}, \code{student_t}, or \code{cauchy} with the 
+#'   \code{autoscale} argument left at its default and recommended value of 
+#'   \code{TRUE}, then the scale(s) of \code{prior} may be tuned internally 
+#'   based on the scales of the predictors. See \code{\link{priors}} for details
+#'   on the rescaling and \code{\link{prior_summary}} for a summary of the 
+#'   priors used for a particular model.)
+#' @param prior_dispersion The prior distribution for the "dispersion" parameter
+#'   (if applicable). The "dispersion" parameter refers to a different parameter
+#'   depending on the \code{family}. For Gaussian models it is the residual SD 
+#'   sigma, for negative binomial models it is the overdispersion parameter, for
+#'   gamma models it is the shape parameter, and for inverse-Gaussian models it 
+#'   is the lambda parameter. Binomial and Poisson models do not have dispersion
+#'   parameters. \code{prior_dispersion} can be a call to \code{exponential} to 
+#'   use an exponential distribution, or \code{normal}, \code{student_t} or 
+#'   \code{cauchy}, which results in a half-normal, half-t, or half-Cauchy 
+#'   prior. See \code{\link{priors}} for details on these functions. To omit 
+#'   a prior ---i.e., to use a flat (improper) uniform prior--- set 
+#'   \code{prior_dispersion} to \code{NULL}.
 #' @param prior_covariance Cannot be \code{NULL}; see \code{\link{decov}} for
 #'   more information about the default arguments.
 #'
@@ -140,6 +156,7 @@ stan_nlmer <- function (formula, data = NULL, subset, weights, na.action, offset
       else SSargs <- cbind(arg1, arg2, SSargs)
     }
     names(SSargs) <- names(formals(FUN))
+    if (FUN == "SSbiexp") SSargs$A1 <- SSargs$A1 + exp(SSargs$A2)
     do.call(FUN, args = SSargs)
   }
   if (SSfun == 5) {
