2018dcc991540862984135b19d16d65857a6bacc
jgabry <jgabry@gmail.com>
2016-12-29 13:51:22 -0500

    roc_plot, class_accuracy, class_error

--- R/predictive-performance.R
+++ R/predictive-performance.R
@@ -9,8 +9,9 @@
 #' 
 #' @templateVar stanregArg object
 #' @template args-stanreg-object
-#' @param psis Should PSIS-weighted estimates also be plotted? May be time
-#'   consuming for very large datasets. Defaults to \code{FALSE}.
+#' @param psis Should PSIS-weighted estimates of out-of-sample performance also 
+#'   be plotted? Recommended but defaults to \code{FALSE} because it may be time
+#'   consuming for very large datasets.
 #' @param ... Currently ignored.
 #' 
 #' @examples 
@@ -25,11 +26,17 @@
 #'  prior = student_t(7, 0, 10), 
 #'  prior_intercept = student_t(7, 0, 10)
 #' )
-#' classification_accuracy(fit) # too optimistic 
-#' classification_accuracy(fit, psis = TRUE) # approximate out-of-sample performance
+#' 
+#' # too optimistic 
+#' class_accuracy(fit) 
+#' class_accuracy(fit, balanced = TRUE) 
+#' 
+#' # approximate out-of-sample performance
+#' class_accuracy(fit, psis = TRUE) 
+#' class_accuracy(fit, psis = TRUE, balanced = TRUE) 
 #' 
 #' roc_plot(fit)
-#' roc_plot(fit, psis = TRUE)
+#' roc_plot(fit, psis = TRUE, print_auc = TRUE)
 #' 
 NULL
 
@@ -38,10 +45,11 @@ NULL
 #' @param balanced Should balanced or unbalanced classification error be
 #'   reported? The default is \code{FALSE} (unbalanced).
 #'   
-#' @return \code{classification_error} and \code{classification_accuracy} return
-#'   a scalar between 0 and 1, which has a custom print method.
+#' @return \code{class_error} returns a scalar between 0 and 1, which has a
+#'   custom print method. \code{class_accuracy} is a simple wrapper around
+#'   \code{class_error} that returns \code{1 - class_error(object)}.
 #' 
-classification_error <-
+class_error <-
   function(object,
            balanced = FALSE,
            psis = FALSE,
@@ -72,15 +80,13 @@ classification_error <-
 
 #' @rdname predictive-performance
 #' @export
-classification_accuracy <-
+class_accuracy <-
   function(object,
            balanced = FALSE,
            psis = FALSE,
            ...) {
-    structure(
-      1 - classification_error(object, balanced, psis, ...), 
-      type = "accuracy"
-    )
+    acc <- 1 - class_error(object, balanced, psis, ...)
+    structure(acc, type = "accuracy")
   }
 
 #' @export
@@ -99,12 +105,12 @@ print.classification_performance <- function(x, digits = 3, ...) {
 #' @rdname predictive-performance
 #' @export 
 #' @param print_auc For \code{roc_plot}, should the area under the curve (AUC)
-#'   be printed on the plot? Defaults to \code{TRUE}.
+#'   be printed in the legend? Defaults to \code{FALSE}.
 #' @param size For \code{roc_plot}, passed to \code{\link[ggplot2]{geom_line}}.
 #' 
 #' @return \code{roc_plot} returns a ggplot object.
 #' 
-roc_plot <- function(object, psis = FALSE, print_auc = TRUE, size = 1, ...) {
+roc_plot <- function(object, psis = FALSE, print_auc = FALSE, size = 1, ...) {
   validate_bernoulli_model(object)
   df <- roc_data(object, psis = psis)
   roc_curve(df, print_auc = print_auc, size = size)
@@ -131,7 +137,6 @@ postpred_probs <- function(x, psis = FALSE) {
   colSums(pps * exp(psis_object$lw_smooth))
 }
 
-
 # @param x stanreg object
 roc_data <- function(x, psis = TRUE) {
   y <- validate_glm_outcome_support(get_y(x), family = binomial())
@@ -152,68 +157,56 @@ roc_data <- function(x, psis = TRUE) {
     out$fpr_loo <- cumsum(!y_loo) / sum(!y_loo)
   }
   
-  return(out)
+  structure(out, psis = isTRUE(psis))
 }
 
+# @param roc_df Object returned by roc_data
+# @param print_auc,size See above.
 #' @importFrom ggplot2 geom_abline geom_line scale_x_reverse scale_color_manual geom_blank
-roc_curve <- function(roc_df, print_auc = TRUE, size = 1) {
-  plot_data <- data.frame(
-    x = 100 * (1 - roc_df$fpr),
-    y = 100 * roc_df$tpr
-  )
-
-  compare_to_psis <- "tpr_loo" %in% colnames(roc_df)
-  if (compare_to_psis) {
-    plot_data$x_loo <- 100 * (1 - roc_df$fpr_loo)
-    plot_data$y_loo <- 100 * roc_df$tpr_loo
-  }
-  
-  clrs <- bayesplot::color_scheme_get()
-  graph <- ggplot(plot_data) +
+roc_curve <- function(roc_df, print_auc = FALSE, size = 1) {
+  compare_to_psis <- isTRUE(attr(roc_df, "psis"))
+  graph <- ggplot(roc_df) +
     geom_abline(
-      intercept = 100,
+      intercept = 0,
       slope = 1,
       size = 0.5,
       linetype = 2,
       color = "gray30"
     ) +
-    geom_line(aes_(x = ~ x, y = ~ y, color = "Posterior ROC"), 
+    ggplot2::geom_line(aes_(x = ~ fpr, y = ~ tpr, color = "Posterior ROC"), 
               size = size)
   
-  caption_txt <- NULL
   scale_labels <- "Posterior ROC"
   if (print_auc)
     scale_labels <- 
-      paste0(scale_labels, 
-             "\n AUC: ", 
-             round(auc_approx(plot_data$x, plot_data$y) / 100, 1),
-             "%\n")
+      paste0(scale_labels, "\n  AUC: ", 
+             round(auc_approx(roc_df$fpr, roc_df$tpr), 2), "\n")
     
-  
   if (compare_to_psis) {
     scale_labels <- c(scale_labels, "PSIS-LOO ROC")
     graph <- graph + 
-      geom_line(aes_(x = ~ x_loo, y = ~ y_loo, color = "PSIS-LOO ROC"), 
+      geom_line(aes_(x = ~ fpr_loo, y = ~ tpr_loo, color = "PSIS-LOO ROC"), 
                 size = 0.75 * size)
     if (print_auc)
       scale_labels[2] <- 
-        paste0(scale_labels[2], 
-               "\n AUC: ", 
-               round(auc_approx(plot_data$x_loo, plot_data$y_loo) / 100, 1),
-               "%\n")
+        paste0(scale_labels[2], "\n  AUC: ", 
+               round(auc_approx(roc_df$fpr_loo, roc_df$tpr_loo), 2), "\n")
   }
   
+  scheme <- bayesplot::color_scheme_get()
   graph + 
     scale_color_manual(
       name = "", 
       labels = scale_labels,
-      values = c("Posterior ROC" = clrs[[5]], "PSIS-LOO ROC" = clrs[[2]])
+      values = c("Posterior ROC" = scheme[[5]], "PSIS-LOO ROC" = scheme[[2]])
     ) + 
-    labs(x = "Specificity (%)", y = "Sensitivity (%)") + #, caption = caption_txt) +
-    scale_x_reverse() +
+    labs(
+      x = "False positive rate \n(1-specificity)", 
+      y = "True positive rate \n(sensitivity)"
+    ) +
     bayesplot::theme_default()
 }
 
 auc_approx <- function(x, y) {
-  -0.5 * sum(diff(x) * (head(y, -1) + tail(y, -1)))
+  0.5 * sum(diff(x) * (head(y, -1) + tail(y, -1)))
 }
