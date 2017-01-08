7b2885cf6b4e2dd08c967bff58ceaa2e070b173d
Ben Goodrich <goodrich.ben@gmail.com>
2017-01-07 22:45:30 -0500

    args-priors.R: mention product_normal prior

--- man-roxygen/args-priors.R
+++ man-roxygen/args-priors.R
@@ -1,7 +1,7 @@
 #' @param prior The prior distribution for the regression coefficients. 
 #'   \code{prior} should be a call to one of the various functions provided by 
 #'   \pkg{rstanarm} for specifying priors. The subset of these functions that 
-#'   can be used for the prior on the coefficients can be grouped into three 
+#'   can be used for the prior on the coefficients can be grouped into several 
 #'   "families":
 #'   
 #'   \tabular{ll}{
@@ -9,6 +9,7 @@
 #'     \emph{Student t family} \tab \code{normal}, \code{student_t}, \code{cauchy} \cr 
 #'     \emph{Hierarchical shrinkage family} \tab \code{hs}, \code{hs_plus} \cr 
 #'     \emph{Laplace family} \tab \code{laplace}, \code{lasso} \cr
+#'     \emph{Product normal family} \tab \code{product_normal} \cr
 #'   }
 #'   
 #'   See the \link[=priors]{priors help page} for details on the families and 
@@ -21,7 +22,7 @@
 #'   family or Laplace family, and if the \code{autoscale} argument to the 
 #'   function used to specify the prior (e.g. \code{\link{normal}}) is left at 
 #'   its default and recommended value of \code{TRUE}, then the default or 
-#'   user-specified prior scale(s) may be tuned internally based on the scales
+#'   user-specified prior scale(s) may be adjusted internally based on the scales
 #'   of the predictors. See the \link[=priors]{priors help page} for details on
 #'   the rescaling and the \code{\link{prior_summary}} function for a summary of
 #'   the priors used for a particular model.
