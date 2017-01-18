fcb48ace46b41dde46c09f18afae4f0b6b27fa63
sambrilleman <louis-george@hotmail.com>
2017-01-18 13:54:58 +1100

    Add unstandardise_quadpoints and unstandardise_quadweights functions to stan_jm.R

--- R/stan_jm.R
+++ R/stan_jm.R
@@ -2653,6 +2653,44 @@ list_nms <- function(object, M) {
   object
 }
 
+# Convert a standardised quadrature node to an unstandardised value based on 
+# the specified integral limits
+#
+# @param x An unstandardised quadrature node
+# @param a The lower limit(s) of the integral, possibly a vector
+# @param b The upper limit(s) of the integral, possibly a vector
+unstandardise_quadpoints <- function(x, a, b) {
+  if (!identical(length(x), 1L) || !is.numeric(x))
+    stop("'x' should be a single numeric value.", call. = FALSE)
+  if (!all(is.numeric(a), is.numeric(b)))
+    stop("'a' and 'b' should be numeric.", call. = FALSE)
+  if (!identical(length(a), length(b)))
+    stop("'a' and 'b' should be vectors of the same length.", call. = FALSE)
+  if (any((b - a) < 0))
+    stop("The upper limits for the integral ('b' values) should be greater than ",
+         "the corresponding lower limits for the integral ('a' values).", call. = FALSE)
+  ((b - a) / 2) * x + ((b + a) / 2)
+}
+
+# Convert a standardised quadrature weight to an unstandardised value based on 
+# the specified integral limits
+#
+# @param x An unstandardised quadrature weight
+# @param a The lower limit(s) of the integral, possibly a vector
+# @param b The upper limit(s) of the integral, possibly a vector
+unstandardise_quadweights <- function(x, a, b) {
+  if (!identical(length(x), 1L) || !is.numeric(x))
+    stop("'x' should be a single numeric value.", call. = FALSE)
+  if (!all(is.numeric(a), is.numeric(b)))
+    stop("'a' and 'b' should be numeric.", call. = FALSE)
+  if (!identical(length(a), length(b)))
+    stop("'a' and 'b' should be vectors of the same length.", call. = FALSE)
+  if (any((b - a) < 0))
+    stop("The upper limits for the integral ('b' values) should be greater than ",
+         "the corresponding lower limits for the integral ('a' values).", call. = FALSE)
+  ((b - a) / 2) * x
+}
+
 
 #-------- The following functions could be moved to priors.R
 
