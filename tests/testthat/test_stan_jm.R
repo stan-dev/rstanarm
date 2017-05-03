b4046a5b15e487f005b7feaf6070b1cb4fd733e2
sambrilleman <louis-george@hotmail.com>
2017-05-03 17:53:15 +1000

    Remove glmer stuff from stan_jm testthat file

--- tests/testthat/test_stan_jm.R
+++ tests/testthat/test_stan_jm.R
@@ -28,7 +28,7 @@ SEED <- 12345
 REFRESH <- ITER
 set.seed(SEED)
 if (interactive()) options(mc.cores = parallel::detectCores())
-TOLSCALE_lmer  <- 0.1 # how many SEs can stan_jm fixefs be from lmer fixefs
+TOLSCALE_lmer  <- 0.2 # how many SEs can stan_jm fixefs be from lmer fixefs
 TOLSCALE_glmer <- 0.3 # how many SEs can stan_jm fixefs be from glmer fixefs
 TOLSCALE_event <- 0.2 # how many SEs can stan_jm fixefs be from coxph fixefs
 FIXEF_tol <- 0.02
@@ -343,88 +343,3 @@ if (interactive()) {
 
 }
 
-
-
-
-
-
-  
-test_that("draws from stan_glmer (gaussian) same as from stan_lmer", {
-  SW(fit1 <- stan_glmer(mpg ~ wt + (1|cyl), data = mtcars, iter = 10, chains = 1, seed = SEED))
-  SW(fit2 <- stan_lmer(mpg ~ wt + (1|cyl), data = mtcars, iter = 10, chains = 1, seed = SEED))
-  expect_identical(as.matrix(fit1), as.matrix(fit2))
-})
-test_that("stan_glmer returns expected result for binomial cbpp example", {
-  links <- c("logit", "probit", "cauchit", "log", "cloglog")
-  # for (i in seq_along(links)) {
-  i <- 1L # it seems only logit gives results similar to glmer with same link 
-  fmla <- cbind(incidence, size - incidence) ~ period + (1 | herd)
-  SW(fit <- stan_glmer(fmla, data = cbpp, family = binomial(links[i]),
-                       chains = CHAINS, iter = ITER, seed = SEED, refresh = REFRESH))
-  expect_stanreg(fit)
-  
-  ans <- glmer(fmla, data = cbpp, family = binomial(links[i]))
-  expect_equal(fixef(fit), fixef(ans), tol = FIXEF_tol)
-  expect_equal(ranef(fit), ranef(ans), tol = RANEF_tol)
-  expect_equal(ngrps(fit), ngrps(ans))
-  # }
-})
-
-context("stan_glmer.nb")
-test_that("stan_glmer.nb ok", {
-  dd <- expand.grid(f1 = factor(1:3),
-                    f2 = LETTERS[1:2], g=1:9, rep=1:15,
-                    KEEP.OUT.ATTRS=FALSE)
-  mu <- 5*(-4 + with(dd, as.integer(f1) + 4*as.numeric(f2)))
-  dd$y <- rnbinom(nrow(dd), mu = mu, size = 0.5)
-  fmla <- as.formula(y ~ f1*f2 + (1|g))
-  SW(fit <- stan_glmer.nb(formula = fmla, data = dd, chains = CHAINS, init_r = 1,
-                          iter = ITER, seed = SEED, refresh = REFRESH))
-  expect_stanreg(fit)
-  
-  ans <- glmer.nb(formula = fmla, data = dd)
-  # ans is messed up
-  # expect_equal(fixef(fit), fixef(ans), tol = FIXEF_tol)
-  # expect_equal(ranef(fit), ranef(ans), tol = RANEF_tol)
-  expect_equal(ngrps(fit), ngrps(ans))
-})
-
-context("stan_lmer")
-test_that("stan_lmer returns expected result for slepstudy example", {
-  fmla <- Reaction / 10 ~ Days + (Days | Subject)
-  SW(fit <- stan_lmer(fmla, data = sleepstudy, refresh = REFRESH,
-                      init_r = 0.05, chains = CHAINS, iter = ITER, seed = SEED))
-  expect_stanreg(fit)
-  
-  ans <- lmer(fmla, data = sleepstudy)
-  expect_equal(fixef(fit), fixef(ans), tol = FIXEF_tol)
-  # expect_equal(ranef(fit), ranef(ans), tol = RANEF_tol)
-  expect_equal(ngrps(fit), ngrps(ans))
-})
-test_that("stan_lmer returns expected result for Penicillin example", {
-  fmla <- as.formula(diameter ~ (1|plate) + (1|sample))
-  SW(fit <- stan_lmer(fmla, data = Penicillin, chains = CHAINS, iter = ITER, 
-                      seed = SEED, refresh = REFRESH, sparse = TRUE))
-  expect_stanreg(fit)
-  
-  ans <- lmer(fmla, data = Penicillin)
-  expect_equal(fixef(fit), fixef(ans), tol = FIXEF_tol)
-  expect_equal(ranef(fit), ranef(ans), tol = RANEF_tol)
-  expect_identical(ngrps(fit), ngrps(ans))
-})
-test_that("stan_lmer ok if global intercept forced to 0", {
-  SW(fit <- stan_lmer(mpg ~ 0 + (1|cyl), data = mtcars, iter = 10, seed = SEED))
-  expect_stanreg(fit)
-})
-test_that("stan_lmer returns an error when multiple group-specific terms are specified", {
-  expect_error(
-    stan_lmer(Reaction / 10 ~ Days + (Days | Subject) + (1|Subject), data = sleepstudy), 
-    "formulas with duplicate group-specific terms"
-  )
-})
-test_that("stan_lmer returns an error when 'family' specified", {
-  expect_error(
-    stan_lmer(Reaction / 10 ~ Days + (Days | Subject), family = "gaussian", data = sleepstudy),
-    "'family' should not be specified"
-  )
-})
