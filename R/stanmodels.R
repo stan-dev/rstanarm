fdfec75713d087721875889b6a18f26c3b92488b
bgoodri <goodrich.ben@gmail.com>
2017-05-07 11:14:27 -0400

    stanmodels.R: add allow_undefined = TRUE

--- R/stanmodels.R
+++ R/stanmodels.R
@@ -28,7 +28,7 @@ stanmodels <- sapply(stan_files, function(f) {
     isystem <- file.path("inst", "chunks")
   if (!file.exists(file.path(isystem, "common_functions.stan")))
     isystem <- file.path("..", "inst", "chunks")
-  stanfit <- rstan::stanc_builder(f, isystem)
+  stanfit <- rstan::stanc_builder(f, isystem, allow_undefined = TRUE)
   stanfit$model_cpp <- list(model_cppname = stanfit$model_name, 
                             model_cppcode = stanfit$cppcode)
   return(do.call(methods::new, args = c(stanfit[-(1:3)], Class = "stanmodel", 
