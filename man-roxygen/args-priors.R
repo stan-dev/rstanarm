12f41a6e28634c5ea1e49b7c971e2728bbafb254
Ben Goodrich <goodrich.ben@gmail.com>
2017-07-02 09:29:05 -0400

    args-priors.R: remove prior_intercept stuff

--- man-roxygen/args-priors.R
+++ man-roxygen/args-priors.R
@@ -26,19 +26,3 @@
 #'   of the predictors. See the \link[=priors]{priors help page} for details on
 #'   the rescaling and the \code{\link{prior_summary}} function for a summary of
 #'   the priors used for a particular model.
-#'   
-#' @param prior_intercept The prior distribution for the intercept. 
-#'   \code{prior_intercept} can be a call to \code{normal}, \code{student_t} or 
-#'   \code{cauchy}. See the \link[=priors]{priors help page} for details on 
-#'   these functions. To omit a prior on the intercept ---i.e., to use a flat
-#'   (improper) uniform prior--- \code{prior_intercept} can be set to
-#'   \code{NULL}.
-#'   
-#'   \strong{Note:} If using a dense representation of the design matrix 
-#'   ---i.e., if the \code{sparse} argument is left at its default value of
-#'   \code{FALSE}--- then the prior distribution for the intercept is set so it
-#'   applies to the value when all predictors are centered. If you prefer to
-#'   specify a prior on the intercept without the predictors being auto-centered,
-#'   then you have to omit the intercept from the \code{\link[stats]{formula}}
-#'   and include a column of ones as a predictor, in which case some element
-#'   of \code{prior} specifies the prior on it, rather than \code{prior_intercept}.
