cd4c2f6921b074764208523024be4c5c80363b95
Sam Brilleman <louis-george@hotmail.com>
2017-09-29 00:28:35 +1000

    More work getting survival predictions for multilevel JM

--- R/log_lik.R
+++ R/log_lik.R
@@ -596,8 +596,12 @@ ll_jm <- function(object, data, pars, include_long = TRUE, include_b = FALSE, su
   ll_event <- ll_event(object, data, pars)
   # log-lik for random effects model
   if (include_b) {
-    if (length(object$cnms) > 1L)
-      stop("Bug found: 'include_b' can only be TRUE when there is one grouping factor.")
+    if (length(object$cnms) > 2L)
+      stop("Bug found: 'include_b' cannot be TRUE when there is more than ",
+           "2 grouping factors.")
+    if (length(object$cnms) == 2L && M > 1)
+      stop("Bug found: 'include_b' cannot be TRUE when there is more than ",
+           "one longitudinal submodel and more than one grouping factor.")
     if ((data$Npat > 1) || (nrow(pars$stanmat) > 1L))
       stop("Bug found: 'include_b' can only be TRUE when 'data' is for one ",
            "individual, and stanmat is for a single draw.")
@@ -605,9 +609,26 @@ ll_jm <- function(object, data, pars, include_long = TRUE, include_b = FALSE, su
     nms    <- unlist(lapply(data$assoc_parts, function(x) x$mod_eta$Z_names))
     b      <- do.call("cbind", pars$b)
     b      <- as.vector(pp_b_ord(b, nms))
-    Sigma  <- VarCorr(object, stanmat = pars$stanmat)[[id_var]]
-    ll_b <- -0.5 * c(determinant(Sigma, logarithm = TRUE)$modulus) + 
-             (b %*% chol2inv(chol(Sigma)) %*% b)[1] + length(b) * log(2 * pi)
+    cnms <- object$cnms
+    id_var <- object$id_var
+    Sigma_id <- VarCorr(object, stanmat = pars$stanmat)[[id_var]]
+    if (length(cnms) > 1L) {
+      b2_var <- grep(glob2rx(id_var), names(cnms), 
+                     value = TRUE, invert = TRUE)
+      Sigma_b2 <- VarCorr(object, stanmat = pars$stanmat)[[b2_var]]
+      Sigma_list <- rep(list(Sigma_b2), data$Ni)
+      which_slot <- which(names(cnms) == b2_var)
+      if (which_slot == 1L) {
+        Sigma_bind <- c(Sigma_list, list(Sigma_id))
+      } else {
+        Sigma_bind <- c(list(Sigma_id), Sigma_list)
+      }
+      Sigma <- as.matrix(Matrix::bdiag(Sigma_bind))
+    } else {
+      Sigma <- Sigma_id
+    }
+    ll_b <- -0.5 * (c(determinant(Sigma, logarithm = TRUE)$modulus) + 
+             (b %*% chol2inv(chol(Sigma)) %*% b)[1] + length(b) * log(2 * pi))
   } else ll_b <- NULL
   # check the dimensions of the various components
   if (is.matrix(ll_event)) { # S * Npat matrices
