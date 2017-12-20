a4d4a9ab44b8b285d75e691a9bc44fd06b3289bc
Ben Goodrich <goodrich.ben@gmail.com>
2017-12-20 01:20:33 -0500

    testthat.R: correct filter again

--- tests/testthat.R
+++ tests/testthat.R
@@ -22,5 +22,5 @@ Sys.unsetenv("R_TESTS")
 example(example_model)
 if (!grepl("^sparc",  R.version$platform)) 
   test_check("rstanarm", invert = TRUE,
-             filter = if (Sys.getenv("NOT_CRAN") != "true") "_[jm,mvmer]$")
+             filter = if (Sys.getenv("NOT_CRAN") != "true") "jm|mvmer")
 
