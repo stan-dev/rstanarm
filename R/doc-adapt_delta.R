6cd9069101949896b8521bd71f10d5a32242fe6a
jgabry <jgabry@gmail.com>
2018-04-07 18:04:17 -0400

    Give R files containing only doc a 'doc' prefix in file name
    [ci skip]

--- R/doc-adapt_delta.R
+++ R/doc-adapt_delta.R
@@ -14,7 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #' Target average acceptance probability
+#' 
+#' Details about the \code{adapt_delta} argument to \pkg{rstanarm}'s modeling
+#' functions.
 #' 
 #' @name adapt_delta
 #' @template reference-stan-manual
