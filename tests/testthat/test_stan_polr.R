9eaf0851fb04ec55a5644e032d0efe5823f73997
Ben Goodrich <goodrich.ben@gmail.com>
2018-09-12 22:20:02 -0400

    test_stan_polr.R: drop test

--- tests/testthat/test_stan_polr.R
+++ tests/testthat/test_stan_polr.R
@@ -61,12 +61,6 @@ test_that("stan_polr runs for esoph example", {
   expect_stanreg(fit2vb)
 })
 
-test_that("stan_polr throws error if formula excludes intercept", {
-  expect_error(stan_polr(tobgp ~ 0 + agegp + alcgp, data = esoph, 
-                         method = "loglog", prior = R2(0.4, "median")), 
-               regexp = "formula not allowed")
-})
-
 test_that("stan_polr throws error if shape,rate specified with >2 outcome levels", {
   expect_error(
     stan_polr(f, data = esoph, method = "loglog", prior = R2(0.4, "median"), shape = 2), 
