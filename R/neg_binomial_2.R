295b45108901bdac968564464a16d8ba3d2a3b8a
jgabry <jgabry@gmail.com>
2019-06-25 14:20:25 -0400

    add beta_binomial() family object
    same links as binomial

--- R/neg_binomial_2.R
+++ R/neg_binomial_2.R
@@ -15,24 +15,34 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-#' Family function for negative binomial GLMs
+#' Additional family functions for GLMs 
 #' 
-#' Specifies the information required to fit a Negative Binomial GLM in a 
-#' similar way to \code{\link[MASS]{negative.binomial}}. However, here the 
-#' overdispersion parameter \code{theta} is not specified by the user and always
-#' estimated (really the \emph{reciprocal} of the dispersion parameter is
-#' estimated). A call to this function can be passed to the \code{family}
-#' argument of \code{\link{stan_glm}} or \code{\link{stan_glmer}} to estimate a
-#' Negative Binomial model. Alternatively, the \code{\link{stan_glm.nb}} and 
-#' \code{\link{stan_glmer.nb}} wrapper functions may be used, which call 
-#' \code{neg_binomial_2} internally.
+#' \pkg{rstanarm} uses several families that are not among the standard 
+#' \link[stats]{family} objects available in \R's \pkg{stats} package.
+#' 
+#' @details
+#' \code{beta_binomial} allows the same link functions as the
+#' \code{\link[stats]{binomial}} family.
+#' 
+#' \code{neg_binomial_2} specifies the information required to fit a Negative
+#' Binomial GLM in a similar way to \code{\link[MASS]{negative.binomial}}.
+#' However, here the overdispersion parameter \code{theta} is not specified by
+#' the user and always estimated (really the \emph{reciprocal} of the dispersion
+#' parameter is estimated). A call to this function can be passed to the
+#' \code{family} argument of \code{\link{stan_glm}} or \code{\link{stan_glmer}}
+#' to estimate a Negative Binomial model. Alternatively, the
+#' \code{\link{stan_glm.nb}} and \code{\link{stan_glmer.nb}} wrapper functions
+#' may be used, which call \code{neg_binomial_2} internally.
 #' 
 #' @export
-#' @param link The same as for \code{\link{poisson}}, typically a character
-#'   vector of length one among \code{"log"}, \code{"identity"}, and
-#'   \code{"sqrt"}.
+#' @param link A string naming the link function. For \code{neg_binomial_2}, the
+#'   options are the same as for \code{\link{poisson}} (defaulting to
+#'   \code{"log"}). For \code{beta_binomial}, the options are the same as for
+#'   \code{\link{binomial}} (defaulting to \code{"logit"}).
 #' @return An object of class \code{\link[stats]{family}} very similar to
-#'   that of \code{\link[stats]{poisson}} but with a different family name.
+#'   that of \code{\link[stats]{poisson}} or \code{\link[stats]{binomial}} 
+#'   but with a different family name.
+#'   
 #' @examples
 #' if (!grepl("^sparc",  R.version$platform))
 #' stan_glm(Days ~ Sex/(Age + Eth*Lrn), data = MASS::quine, seed = 123,
@@ -44,6 +54,26 @@ neg_binomial_2 <- function(link = "log") {
   out <- poisson(link)
   out$family <- "neg_binomial_2"
   out$variance <- function(mu, theta = Inf) mu + mu^2 / theta
+  out$dev.resids <- function(y, mu, wt) {
+    stop("'dev.resids' function should not be called")
+  }
+  out$aic <- function(y, n, mu, wt, dev) {
+    stop("'aic' function should not have been called")
+  }
+  out$simulate <- function(object, nsim)
+    stop("'simulate' function should not have been called")
+  return(out)
+}
+
+#' @rdname neg_binomial_2
+#' @export
+beta_binomial <- function(link = "logit") {
+  out <- binomial(link)
+  out$family <- "beta_binomial"
+  out$
+  out$variance <- function(mu, phi) {
+    stop("'variance' function should not be called")
+  }
   out$dev.resids <- function(y, mu, wt) {
     stop("'dev.resids' function should not be called")
   }
