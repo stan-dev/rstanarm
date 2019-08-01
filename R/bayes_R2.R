stan-dev/rstanarm

commit 9ff7f71361557091413c3949c8d601236cf456d0

Update bayes_R2.R
* fix for binomial case with trials > 1

R/bayes_R2.R
@@ -50,11 +50,16 @@ bayes_R2.stanreg <- function(object, ..., re.form = NULL) {
       y <- get_y(object)
       if (NCOL(y) == 2) {
         trials <- rowSums(y)
-        mu_pred <- mu_pred %*% diag(trials)
+        trials_mat <- matrix(trials, nrow = nrow(mu_pred), ncol = ncol(mu_pred), 
+                             byrow = TRUE)
+        tmp <- mu_pred * trials_mat
+        sigma2 <- rowMeans(tmp * (1 - mu_pred))
+        mu_pred <- tmp
+      } else {
+        sigma2 <- rowMeans(mu_pred * (1 - mu_pred) * trials_mat)
       }
-      sigma2 <- rowMeans(mu_pred * (1 - mu_pred))
     } else {
-      sigma2 <- drop(as.matrix(fit, pars = "sigma"))^2
+      sigma2 <- drop(as.matrix(object, pars = "sigma"))^2
     }
     
     var_mu_pred <- apply(mu_pred, 1, var)
@@ -79,21 +84,23 @@ loo_R2.stanreg <- function(object, ...) {
     stop("loo_R2 is only available for Gaussian and binomial models.")
   }
   
-  y <- get_y(fit)
-  log_ratios <- -log_lik(fit)
-  psis_object <- fit[["loo"]][["psis_object"]]
+  y <- get_y(object)
+  log_ratios <- -log_lik(object)
+  psis_object <- object[["loo"]][["psis_object"]]
   if (is.null(psis_object)) {
     psis_object <- loo::psis(log_ratios, r_eff = NA)
   }
   
-  mu_pred <- posterior_linpred(fit, transform = TRUE)
+  mu_pred <- posterior_linpred(object, transform = TRUE)
   if (is.binomial(fam)) {
     if (is.factor(y)) {
       y <- fac2bin(y)
     } else if (NCOL(y) == 2) {
       trials <- rowSums(y)
       y <- y[, 1]
-      mu_pred <- mu_pred %*% diag(trials)
+      trials_mat <- matrix(trials, nrow = nrow(mu_pred), ncol = ncol(mu_pred), 
+                           byrow = TRUE)
+      mu_pred <- mu_pred * trials_mat
     }
   }
   mu_pred_loo <- loo::E_loo(mu_pred, psis_object, log_ratios = log_ratios)$value

commit aeab55a4139e5bd9702b8f8f36e168ab4c0adc0f

Fix bug in bayes_R2() for binomial family with bernoulli trials
This closes #376, fixing a bug introduced in 9ff7f71.

R/bayes_R2.R
@@ -58,7 +58,7 @@ bayes_R2.stanreg <- function(object, ..., re.form = NULL) {
         sigma2 <- rowMeans(tmp * (1 - mu_pred))
         mu_pred <- tmp
       } else {
-        sigma2 <- rowMeans(mu_pred * (1 - mu_pred) * trials_mat)
+        sigma2 <- rowMeans(mu_pred * (1 - mu_pred))
       }
     } else {
       sigma2 <- drop(as.matrix(object, pars = "sigma"))^2