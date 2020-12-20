ab039e53124c65137f0d98b9a8fcbf39aa43dcc5
jgabry <jgabry@gmail.com>
2020-12-19 17:58:07 -0700

    see if stan_jm doesn't segfault on linux

--- tests/testthat/test_stan_jm.R
+++ tests/testthat/test_stan_jm.R
@@ -19,7 +19,6 @@
 # tests can be run using devtools::test() or manually by loading testthat 
 # package and then running the code below possibly with options(mc.cores = 4).
 
-if (!isTRUE(as.logical(Sys.getenv("CI")))) {
 library(rstanarm)
 library(lme4)
 library(survival)
@@ -554,6 +553,4 @@ for (j in c(1:30)) {
       expect_survfit(ps)
     })
   }
-}
-
 }
