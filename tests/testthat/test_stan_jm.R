083a1ed8fd5f2ef8621806544a728bd5ecf0d359
jgabry <jgabry@gmail.com>
2021-01-04 10:57:30 -0700

    remove outcome from newdata for test

--- tests/testthat/test_stan_jm.R
+++ tests/testthat/test_stan_jm.R
@@ -521,13 +521,13 @@ for (j in c(1:30)) {
     }) 
     
     test_that("posterior_traj works with new data with and without offset", {
-      nd <- pbcLong[1:2, ]
+      pbcLong2 <- pbcLong
+      nd <- pbcLong2[1:2, c("id", "year")]
       expect_s3_class(posterior_traj(f1, newdataLong = nd, dynamic = FALSE), "predict.stanjm")
       
-      pbcLong2 <- pbcLong
       pbcLong2$off <- 1
       o <- SW(f1_off <- update(f1, dataLong = pbcLong2, formulaLong. = logBili ~ offset(off) + year + (year | id)))
-      nd <- pbcLong2[1:2, ]
+      nd <- pbcLong2[1:2, c("id", "year", "off")]
       expect_s3_class(posterior_traj(f1_off, newdataLong = nd, dynamic = FALSE), "predict.stanjm")
     })
     
