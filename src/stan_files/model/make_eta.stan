1516ce87ac3434a6ebeac0d92a47047d88367824
Ben Goodrich <goodrich.ben@gmail.com>
2022-03-14 14:53:13 -0400

    remove csr_matrix_times_vector2

--- src/stan_files/model/make_eta.stan
+++ src/stan_files/model/make_eta.stan
@@ -1,7 +1,7 @@
   vector[N] eta;  // linear predictor
   if (K > 0) {
     if (dense_X) eta = X[1] * beta;
-    else eta = csr_matrix_times_vector2(N, K, w_X, v_X, u_X, beta);
+    else eta = csr_matrix_times_vector(N, K, w_X, v_X, u_X, beta);
   }
   else eta = rep_vector(0.0, N);
   if (has_offset == 1) eta += offset_;
--- tests/testthat/test_stan_functions.R
+++ tests/testthat/test_stan_functions.R
@@ -50,9 +50,6 @@ functions <- sapply(dir(MODELS_HOME, pattern = "stan$", full.names = TRUE), func
   } else return(as.character(NULL))
 })
 names(functions) <- basename(names(functions))
-functions$polr.stan <- grep("csr_matrix_times_vector2", 
-                            functions$polr.stan, 
-                            value = TRUE, fixed = TRUE, invert = TRUE)
 functions <- c(unlist(lapply(file.path(MODELS_HOME, "functions", 
                              c("common_functions.stan",
                                "bernoulli_likelihoods.stan",
@@ -399,9 +396,7 @@ if (require(lme4) && require(HSAUR3)) test_that("the Stan equivalent of lme4's Z
                  tol = 1e-14)
     
     parts <- extract_sparse_parts(Z)
-    Zb <- csr_matrix_times_vector2_test(nrow(Z), ncol(Z), parts$w, 
-                                        parts$v - 1L, parts$u - 1L, b)
-    expect_equal(Zb, as.vector(Z %*% b), tol = 1e-14)
+    Zb <- Z %*% b
     if (all(sapply(group$cnms, FUN = function(x) {
         length(x) == 1 && x == "(Intercept)"
       })) ) {
