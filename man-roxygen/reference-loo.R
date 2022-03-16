f5ca87fef3f3baf7810f2fde09c45a65cbcb23eb
Ben Goodrich <goodrich.ben@gmail.com>
2022-03-16 17:27:22 -0400

    more URL fixes

--- man-roxygen/reference-loo.R
+++ man-roxygen/reference-loo.R
@@ -3,9 +3,8 @@
 #'   Bayesian model evaluation using leave-one-out cross-validation and WAIC.
 #'   \emph{Statistics and Computing}. 27(5), 1413--1432.
 #'   doi:10.1007/s11222-016-9696-4. arXiv preprint:
-#'   \url{https://arxiv.org/abs/1507.04544/}
+#'   \url{https://arxiv.org/abs/1507.04544}
 #'
 #'   Yao, Y., Vehtari, A., Simpson, D., and Gelman, A. (2018) Using
 #'   stacking to average Bayesian predictive distributions. \emph{Bayesian
-#'   Analysis}, advance publication,  doi:10.1214/17-BA1091.
-#'   (\href{https://projecteuclid.org/euclid.ba/1516093227}{online}).
+#'   Analysis}, advance publication,  \doi{10.1214/17-BA1091}.
